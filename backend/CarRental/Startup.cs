a64e0fca41be80d16eb58353de4522bb8e3018a5
Maciej Łukasik <maciej.lukasik@valueadd.pl>
2021-10-16 20:08:59 +0200

    CRA-182: Add basic project structure

--- backend/CarRental/Startup.cs
+++ backend/CarRental/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -41,10 +42,15 @@ namespace CarRental
             }
             
             app.UseRouting();
-
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello world!");
+                });
+            });
         }
     }
 }
